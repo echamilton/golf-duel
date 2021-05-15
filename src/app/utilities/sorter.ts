99fde6d7828fe29de432d381dabcc8ce464d1c9b
Evan Hamilton <evanchamilton@gmail.com>
2021-05-15 03:25:14 -0400

    fix sort

--- src/app/utilities/sorter.ts
+++ src/app/utilities/sorter.ts
@@ -1,12 +1,12 @@
 export function sortScores(data: any) {
-  return data.sort((a, b) => {
+  return data.sort((a: any, b: any) => {
     switch ('score') {
       case 'score':
         return compare(+a.score, +b.score, true);
       default:
         return 0;
     }
-    function compare(a, b, isAsc) {
+    function compare(a: number, b: number, isAsc: boolean) {
       return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
     }
   });
