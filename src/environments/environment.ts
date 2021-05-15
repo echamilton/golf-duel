b22e4ce514fada10994c3f3937466513c18b93af
Evan Hamilton <evanchamilton@gmail.com>
2021-05-15 00:15:49 -0400

    initial set of changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: 'AIzaSyABOO_Xt0f-Y52mib9D79Kog95osDxuY50',
+    authDomain: 'legbreaker-app.firebaseapp.com',
+    databaseURL: 'https://legbreaker-app.firebaseio.com',
+    projectId: 'legbreaker-app',
+    storageBucket: 'legbreaker-app.appspot.com',
+    messagingSenderId: '727660903518'
+  }
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
