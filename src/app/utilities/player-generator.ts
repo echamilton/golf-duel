f92781f008a9ef46ae9b8b23bcaef430ef24d58a
Evan Hamilton <evanchamilton@gmail.com>
2021-05-16 10:41:39 -0400

    update

--- src/app/utilities/player-generator.ts
+++ src/app/utilities/player-generator.ts
@@ -1,12 +1,12 @@
 import { IPlayer } from '../models/models';
 import { golfersFile } from './players';
-import * as cloneDeep from 'lodash/cloneDeep';
+import * as cloneDeep from 'lodash';
 
-export function buildPlayerList(golfersFromEspn: IPlayer[]):void {
+export function buildPlayerList(golfersFromEspn: IPlayer[]): void {
   const file = cloneDeep(golfersFile);
-  file.forEach(file => {
-    const golfer = golfersFromEspn.find(x => x.name == file.name)
-    if(golfer){
+  file.forEach((file) => {
+    const golfer = golfersFromEspn.find((x) => x.name == file.name);
+    if (golfer) {
       file.golferId = Number(golfer.golferId);
     }
   });
